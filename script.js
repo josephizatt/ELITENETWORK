a5f0b37eb78a406595a445d3cda42337a5d25ab8
joe <joe@izatt.co.uk>
2023-05-19 12:23:58 +0100

    updates

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.getElementById('inviteButton').addEventListener('click', function() {
+
+  document.getElementById('inviteButton').addEventListener('click', function() {
+    window.location.href = 'https://discord.gg/vYDFRESnNn';
+  });
+  
+  document.getElementById('navInviteButton').addEventListener('click', function() {
     window.location.href = 'https://discord.gg/vYDFRESnNn';
   });
   
