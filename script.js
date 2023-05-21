2e825d9084d1eb61f95e28a5c72ea47b260e18bd
joe <joe@izatt.co.uk>
2023-05-21 14:49:31 +0100

    update

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 function getRandomPosition(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function animateBackgroundImages() {
-  const images = document.querySelectorAll('.background-images img');
-  const totalImages = images.length;
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
   
-  let delay = 0;
-  let zIndex = 1;
-
-  images.forEach((img) => {
-    const minDelay = 0;
-    const maxDelay = 3000;
-    const minTranslate = -500;
-    const maxTranslate = 500;
-
-    img.style.animationDelay = `${delay}ms`;
-    img.style.zIndex = zIndex;
-
-    const translateX = getRandomPosition(minTranslate, maxTranslate);
-    const translateY = getRandomPosition(minTranslate, maxTranslate);
-
-    img.style.transform = `translate(${translateX}px, ${translateY}px)`;
-    
-    delay += getRandomPosition(minDelay, maxDelay);
-    zIndex++;
-
-    img.addEventListener('animationiteration', () => {
-      const newTranslateX = getRandomPosition(minTranslate, maxTranslate);
-      const newTranslateY = getRandomPosition(minTranslate, maxTranslate);
-      img.style.transform = `translate(${newTranslateX}px, ${newTranslateY}px)`;
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  
+  function createBackgroundImages() {
+    const container = document.querySelector('.background-images');
+    let images = Array.from(container.querySelectorAll('img'));
+    const totalImages = images.length;
+  
+    shuffleArray(images);
+  
+    images.forEach((image, index) => {
+      image.style.zIndex = index;
+      image.style.transform = `translateY(${index * 100}%)`;
     });
-  });
-}
-
-animateBackgroundImages();
+  
+    setInterval(() => {
+      const randomIndex = getRandomPosition(0, totalImages - 1);
+      const image = images[randomIndex];
+  
+      image.style.zIndex = totalImages;
+      images.forEach((img) => {
+        if (img !== image && img.style.zIndex > 0) {
+          img.style.zIndex = img.style.zIndex - 1;
+        }
+      });
+    }, 5000);
+  
+    setInterval(() => {
+      images.forEach((image) => {
+        const currentTranslateY = parseFloat(image.style.transform.match(/[-]{0,1}[\d]*[.]{0,1}[\d]+/g)[0]);
+        image.style.transform = `translateY(${(currentTranslateY - 2) % (totalImages * 100)}%)`;
+      });
+    }, 50); // Adjust this value to control the speed of scrolling
+  }
+  
+  createBackgroundImages();
+  
